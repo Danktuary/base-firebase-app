8504deccae9388c40ac380eb067152b3947191fd
Sanctuary <sanc.pw@gmail.com>
2020-10-12 20:55:17 -0400

    Add firebase db file with env config

---
+++ src/db.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const db = firebase.initializeApp({
+	apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+	authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.VUE_APP_FIREBASE_SENDER_ID,
+	appId: process.env.VUE_APP_FIREBASE_APP_ID,
+}).firestore()
+
+export default db
